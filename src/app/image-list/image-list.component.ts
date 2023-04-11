bc2c2224a585907e2f2902d829139ab5505e9f44
mastrong24 <117696864+mastrong24@users.noreply.github.com>
2023-04-11 13:48:37 +0800

    3 files modified, 3 added

---
+++ src/app/image-list/image-list.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-list',
+  templateUrl: './image-list.component.html',
+  styleUrls: ['./image-list.component.css']
+})
+export class ImageListComponent {
+  
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
